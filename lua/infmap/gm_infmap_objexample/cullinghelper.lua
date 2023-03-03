7033f8bedfc3b7f14b9f265eda2cfe7e149a3d7e
hl2jeep <51200953+hl2jeep@users.noreply.github.com>
2023-03-03 09:41:39 +0000

    Add files via upload

---
+++ lua/infmap/gm_infmap_objexample/cullinghelper.lua
@@ -0,0 +1,39 @@
+InfMap.parsed_object_names = InfMap.parsed_object_names or {}
+--InfMap.parsed_object_lod_list = InfMap.parsed_object_lod_list or {}
+local playervecstate = Vector()
+--InfMap.globallocalplayerlmao = InfMap.globallocalplayerlmao
+--InfMap.stupidbullshitfuckyou = InfMap.stupidbullshitfuckyou
+function clearTable(t)
+    for k in pairs(t) do
+      t[k] = nil
+    end
+end
+
+hook.Add("PostDrawOpaqueRenderables", "this_is_retarded", function()
+    if LocalPlayer().CHUNK_OFFSET != playervecstate then
+        print("running")
+        InfMap.parsed_object_lod_list = {}
+        for i = 1, #InfMap.parsed_object_names do
+            objectname2222 = InfMap.parsed_object_names[i]
+            if Vector(objectname2222.name):DistToSqr(LocalPlayer().CHUNK_OFFSET - Vector(1,1,1),LocalPlayer().CHUNK_OFFSET) < 6 then
+                table.insert(InfMap.parsed_object_lod_list, {
+                    isculled = true,
+                    originobjname = objectname2222.objname
+                })
+                print("truecull")
+            else
+                table.insert(InfMap.parsed_object_lod_list, {
+                    isculled = false,
+                    originobjname = objectname2222.objname
+        
+                })
+                print("falsecull")
+            end
+            print("objname: ", objectname2222.objname)
+            print("culled: ", InfMap.parsed_object_lod_list.isculled)
+            print("table first entry: ", InfMap.parsed_object_lod_list.originobjname)
+            print("table second entry: ", InfMap.parsed_object_lod_list[2])
+            playervecstate = LocalPlayer().CHUNK_OFFSET
+        end
+    end
+end)
---
+++ lua/infmap/gm_infmap_objexample/loadobjchunktable.lua
@@ -0,0 +1,12 @@
+InfMap.parsed_object_names = InfMap.parsed_object_names or {}
+
+InfMap.parsed_object_names = {
+    {
+        name = Vector(0,0,0),
+        objname = "treesnshitcombined"
+    },
+    {
+        name = Vector(0,0,0),
+        objname = "buildingsandshitcombined"
+    }
+}
