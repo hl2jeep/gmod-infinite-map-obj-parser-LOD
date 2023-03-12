6ff37222cb80068255abf6af88177a35b9e76fcd
hl2jeep <51200953+hl2jeep@users.noreply.github.com>
2023-03-12 19:37:55 +0000

    fixed fucked shit

--- lua/autorun/loadobjectobjexample.lua
+++ lua/autorun/loadobjectobjexample.lua
@@ -1,24 +1,24 @@
+if game.GetMap() == "gm_infmap_objexample" then 
+    // Clear pervious data if it exists
+    InfMap.clear_parsed_objects()
+    // Create a matrix to scale the model
+    local mat = Matrix()
+    mat:SetTranslation(Vector(0, 0, 0))		// Sets the models position
+    mat:SetAngles(Angle(0, 0, 0))			// Sets the models rotation
+    mat:SetScale(Vector(1, 1, 1) * 39.3701)	// Conversion from Meters to Inches
+    // visuals = 1
+    // collision = 2
+    // nocull Shader = No backface culling
+    // alphatest Shader = Enable transparency on the model
 
-// Clear pervious data if it exists
-InfMap.clear_parsed_objects()
-// Create a matrix to scale the model
-local mat = Matrix()
-mat:SetTranslation(Vector(0, 0, 0))		// Sets the models position
-mat:SetAngles(Angle(0, 0, 0))			// Sets the models rotation
-mat:SetScale(Vector(1, 1, 1) * 39.3701)	// Conversion from Meters to Inches
-// visuals = 1
-// collision = 2
-// nocull Shader = No backface culling
-// alphatest Shader = Enable transparency on the model
-
-// Create our visual model, note the "1" which means it is ONLY visual
-InfMap.parse_obj("0_0_0", mat, 1, "nocull")
+    // Create our visual model, note the "1" which means it is ONLY visual
+    InfMap.parse_obj("0_0_0", mat, 1, "nocull")
 
 
-// Create our physical model, note the "2" which means it is ONLY collision
-// Because it is collision, we do not need the shader option
-//InfMap.parse_obj("cloud_collision", mat, 2)
-    
-// If you wish to have collision AND visuals combined, you can do so by using "0" or "3"
-// Example: InfMap.parse_obj("cloud", mat, 3, "nocull alphatest"
-
+    // Create our physical model, note the "2" which means it is ONLY collision
+    // Because it is collision, we do not need the shader option
+    //InfMap.parse_obj("cloud_collision", mat, 2)
+        
+    // If you wish to have collision AND visuals combined, you can do so by using "0" or "3"
+    // Example: InfMap.parse_obj("cloud", mat, 3, "nocull alphatest"
+end
